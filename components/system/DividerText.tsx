17ce60f6bfd2cbf8895a0eaf814f8f4439e328d0
kjmin-dev <kjmin.dev@gmail.com>
2022-08-04 23:38:09 +0900

    Impl DividerText

---
+++ components/system/DividerText.tsx
@@ -0,0 +1,17 @@
+import { PropsWithChildren } from 'react';
+
+export interface DividerTextProps {
+  spacing?: number;
+}
+
+export default function DividerText({
+  spacing = 2,
+  children,
+}: PropsWithChildren<DividerTextProps>) {
+  return (
+    <div className="relative flex py-5 items-center">
+      {children}
+      <div className={`flex-grow border-t border-gray-400 ml-${spacing}`}></div>
+    </div>
+  );
+}
