d50c5aef057f765a2909727f2ef81464748c21fb
kjmin-dev <kjmin.dev@gmail.com>
2022-08-07 22:50:41 +0900

    specify daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js', './pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   plugins: [require('@tailwindcss/typography'), require('daisyui')],
   theme: {
     extend: {},
   },
-}
+  daisyui: {
+    themes: ['light', 'dark'],
+  },
+};
